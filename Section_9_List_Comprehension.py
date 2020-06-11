2e9676c4f559a015450d3de264488932358ed929
Vonn Johnson <vonn.johnson@outlook.com>
2020-06-11 14:24:59 -0400

    Section 9 Completed

--- Section_9_List_Comprehension.py
+++ Section_9_List_Comprehension.py
@@ -17,3 +17,45 @@ temps = [221, 234, 340, -9999, 230]
 new_temps = [temp/10 for temp in temps if temp != -9999] # we will create variable called temp that will be divided by 10. For each item in the list temps we will call it temp and this will only apply to items that are not equal to -9999
 
 print(new_temps)
+
+# coding exercise (create a list that only prints out numbers)
+def list_func(list_):
+    new_list = [num for num in list_ if isinstance(num, int) or isinstance(num, float) == True]
+    return new_list
+
+list_ = [99,'no data', 95.0, 94, 'no data','93']
+print(list_func(list_))
+
+# coding exercise (only return numbers greater than 0)
+def list_func(list_):
+    new_list = [num for num in list_ if num > 0]
+    return new_list
+
+list_ = [-5,3,-1, 101]
+print(list_func(list_))
+
+# List Comprehension with if-Esle Conditional
+
+# for if else statements inside list comprehension always remember that the for loop goes at that end. Should look like this
+
+temps = [221, 234, 340, -9999, 230]
+
+new_temps = [temp/10 if temp != -9999 else 0 for temp in temps]
+# divide the value by 10 if the value is not -9999, if it is -9999 make it 0
+print(new_temps)
+
+# coding exercise (list of strings and numbers, but turn strings into 0)
+def list_func(list_):
+    new_list = [num if isinstance(num, int) else 0 for num in list_]
+    return new_list
+
+list_ = [99,'no data', 95.0, 94, 'no data','93']
+print(list_func(list_))
+
+# coding exercise (sum up floats that are string data types)
+def list_func(list_):
+    new_list = [float(num) if isinstance(num, str) else num for num in list_]
+    return sum(new_list)
+
+list_ = ['1.2','2.6','3.3']
+print(list_func(list_))
